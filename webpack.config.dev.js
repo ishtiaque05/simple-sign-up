bc258f74e6a71b7756d906bb53731cb29a5213c1
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-09-19 20:23:47 +0600

    correct typo and successfully configured webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,35 @@
-import path from 'path';
+import path from 'path'
+import webpack from 'webpack';
+
 export default {
-  devtools: 'eval-source-map',
-  entry: path.join(__dirname, '/client/index.js'),
-  output:{
-    path: '/'
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: '/',
+    publicPath: '/',
+    filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
-        include: path.join(__dirname, 'client'),
-        loaders: ['babel']
+        include: [
+          path.join(__dirname, 'client'),
+          path.join(__dirname, 'server/shared')
+        ],
+        loaders: [ 'react-hot-loader', 'babel-loader' ]
       }
     ]
   },
   resolve: {
-    extension: ['', '.js']
+    extensions: [ '*', '.js' ]
   }
 }
